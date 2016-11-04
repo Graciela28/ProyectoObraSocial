712d2cb3ff66664dbf5715e5a76194863577742d
Martin Navarro <mnavarromugas@yahoo.com.ar>
2016-11-04 11:05:51 -0300

    Agrega rangos al reporte estadistico de practicas

--- GUILayer/Reportes/Frm_ReporteEstadisticoDePracticas.Designer.vb
+++ GUILayer/Reportes/Frm_ReporteEstadisticoDePracticas.Designer.vb
@@ -24,12 +24,27 @@ Partial Class Frm_ReporteEstadisticoDePracticas
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
-        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
-        Me.DataSet4 = New primeraEntrega.DataSet4()
         Me.EstadisticaPracticasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        CType(Me.DataSet4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DataSet4 = New primeraEntrega.DataSet4()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.dtp_desde = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dtp_hasta = New System.Windows.Forms.DateTimePicker()
+        Me.btn_generar = New System.Windows.Forms.Button()
         CType(Me.EstadisticaPracticasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSet4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'EstadisticaPracticasBindingSource
+        '
+        Me.EstadisticaPracticasBindingSource.DataMember = "EstadisticaPracticas"
+        Me.EstadisticaPracticasBindingSource.DataSource = Me.DataSet4
+        '
+        'DataSet4
+        '
+        Me.DataSet4.DataSetName = "DataSet4"
+        Me.DataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'ReportViewer1
         '
@@ -40,35 +55,77 @@ Partial Class Frm_ReporteEstadisticoDePracticas
         ReportDataSource1.Value = Me.EstadisticaPracticasBindingSource
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "primeraEntrega.EstadisticaPracticas.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(12, 12)
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 51)
         Me.ReportViewer1.Name = "ReportViewer1"
-        Me.ReportViewer1.Size = New System.Drawing.Size(601, 442)
+        Me.ReportViewer1.Size = New System.Drawing.Size(601, 403)
         Me.ReportViewer1.TabIndex = 0
         '
-        'DataSet4
+        'dtp_desde
         '
-        Me.DataSet4.DataSetName = "DataSet4"
-        Me.DataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        Me.dtp_desde.Location = New System.Drawing.Point(15, 25)
+        Me.dtp_desde.Name = "dtp_desde"
+        Me.dtp_desde.Size = New System.Drawing.Size(200, 20)
+        Me.dtp_desde.TabIndex = 1
         '
-        'EstadisticaPracticasBindingSource
+        'Label1
         '
-        Me.EstadisticaPracticasBindingSource.DataMember = "EstadisticaPracticas"
-        Me.EstadisticaPracticasBindingSource.DataSource = Me.DataSet4
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Desde:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(218, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Hasta:"
+        '
+        'dtp_hasta
+        '
+        Me.dtp_hasta.Location = New System.Drawing.Point(221, 25)
+        Me.dtp_hasta.Name = "dtp_hasta"
+        Me.dtp_hasta.Size = New System.Drawing.Size(200, 20)
+        Me.dtp_hasta.TabIndex = 3
+        '
+        'btn_generar
+        '
+        Me.btn_generar.Location = New System.Drawing.Point(427, 22)
+        Me.btn_generar.Name = "btn_generar"
+        Me.btn_generar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_generar.TabIndex = 5
+        Me.btn_generar.Text = "Generar"
+        Me.btn_generar.UseVisualStyleBackColor = True
         '
         'Frm_ReporteEstadisticoDePracticas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(625, 466)
+        Me.Controls.Add(Me.btn_generar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.dtp_hasta)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dtp_desde)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "Frm_ReporteEstadisticoDePracticas"
         Me.Text = "Frm_ReporteEstadisticoDePracticas"
-        CType(Me.DataSet4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.EstadisticaPracticasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSet4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
     Friend WithEvents EstadisticaPracticasBindingSource As System.Windows.Forms.BindingSource
     Friend WithEvents DataSet4 As primeraEntrega.DataSet4
+    Friend WithEvents dtp_desde As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents dtp_hasta As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btn_generar As System.Windows.Forms.Button
 End Class
--- GUILayer/Reportes/Frm_ReporteEstadisticoDePracticas.vb
+++ GUILayer/Reportes/Frm_ReporteEstadisticoDePracticas.vb
@@ -3,8 +3,18 @@
 Public Class Frm_ReporteEstadisticoDePracticas
 
     Private Sub Frm_ReporteEstadisticoDePracticas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub btn_generar_Click(sender As Object, e As EventArgs) Handles btn_generar.Click
+        If (dtp_desde.Value > dtp_hasta.Value) Then
+            MsgBox("Periodo incorrecto", MsgBoxStyle.Exclamation)
+            Exit Sub
+        End If
+
+        'ReportViewer1.LocalReport.SetParameters({New ReportParameter("pFechaDesde", dtp_desde.Value), New ReportParameter("pFechaHasta", dtp_hasta.Value)})
         Dim tabla As DataTable
-        Dim consulta = "select T1.nombre as n_practica, COUNT(T2.id_practica) as ctd, (SELECT COUNT (T2.id_practica) * 100/ (SELECT COUNT(T3.id_practica) from DetallesAtencion T3)) as porcentaje FROM DetallesAtencion T2, Practicas T1 where T2.id_practica = T1.id GROUP BY T1.nombre"
+        Dim consulta = "select T1.nombre as n_practica, COUNT(T2.id_practica) as ctd, (SELECT COUNT (T2.id_practica) * 100/ (SELECT COUNT(T3.id_practica) from DetallesAtencion T3)) as porcentaje FROM DetallesAtencion T2, Practicas T1 where T2.id_practica = T1.id AND fecha_alta BETWEEN CONVERT(datetime, '" + dtp_desde.Value.ToString("MM/dd/yyyy") + "', 103) AND CONVERT(datetime, '" + dtp_hasta.Value.ToString("MM/dd/yyyy") + "',103) GROUP BY T1.nombre"
         Me.ReportViewer1.LocalReport.DataSources.Clear()
         tabla = BDHelper.ConsultaSQL(consulta)
         Me.ReportViewer1.LocalReport.DataSources.Add(New ReportDataSource("DataSetEstadistica1", tabla))
